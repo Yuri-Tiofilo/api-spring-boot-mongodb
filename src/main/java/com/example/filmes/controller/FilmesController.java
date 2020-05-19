6bbc4af9355333e76a8ce57b2fb18dfa3711d8c3
Yuri Silva <yusilva12345@gmail.com>
2020-05-18 22:06:15 -0300

    CRUD filmes

--- src/main/java/com/example/filmes/controller/FilmesController.java
+++ src/main/java/com/example/filmes/controller/FilmesController.java
@@ -7,21 +7,21 @@ import com.example.filmes.entities.Filme;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
 @RequestMapping("/api/filme")
 public class FilmesController {
-  // GET
-
   ArrayList<Filme> filmes = new ArrayList<Filme>();
 
-  @GetMapping
+  // GET
   public ArrayList<Filme> getFilme() {
 
     return this.filmes;
@@ -35,14 +35,39 @@ public class FilmesController {
     return filme;
   }
 
-  @PutMapping(value="/api/filme/:id")
-  public Filme putMethodName(@PathVariable String id, @RequestBody Filme filme) {
-      //TODO: process PUT request
-      
-    return filme;
+  // DELETE
+  @DeleteMapping("/{id}")
+  public Filme deleteFilme(@PathVariable String id) {
+    Filme newFilme = null;
+
+    for (Filme filme: this.filmes) {
+      if(filme.getId().equals(id)) {
+
+        newFilme = filme;
+      }
+    }
+    if(newFilme != null) {
+      filmes.remove(newFilme);
+
+      return newFilme;
+    }
+    else return null;
   }
 
+  // UPDATE
+  @PutMapping
+  public Filme updateFilme(@RequestBody Filme newFilme) {
+      
+    for (Filme filme: this.filmes) {
+      if(filme.getId().equals(newFilme.getId())) {
+        int indexExist = this.filmes.indexOf(filme);
 
+        this.filmes.set(indexExist, newFilme);
 
-  
+        return newFilme;
+      }
+    }
+    return null;
+
+  }
 }
